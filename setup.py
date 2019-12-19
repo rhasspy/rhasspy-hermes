2be2ee01717a325b4fc0b02bdacf2c1ccf13b912
Michael Hansen <hansen.mike@gmail.com>
2019-12-19 10:05:00 -0500

    Add VERSION file

--- setup.py
+++ setup.py
@@ -5,16 +5,25 @@ this_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_dir, "README.md"), "r") as readme_file:
     long_description = readme_file.read()
 
+with open("requirements.txt", "r") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open("VERSION", "r") as version_file:
+    version = version_file.read().strip()
+
 setuptools.setup(
     name="rhasspy-hermes",
-    version="0.1.1",
+    version=version,
     author="Michael Hansen",
     author_email="hansen.mike@gmail.com",
     url="https://github.com/synesthesiam/rhasspy-hermes",
     packages=setuptools.find_packages(),
-    install_requires=["attrs"],
-    classifiers=["Programming Language :: Python :: 3"],
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    entry_points={},
+    python_requires=">=3.6",
 )
