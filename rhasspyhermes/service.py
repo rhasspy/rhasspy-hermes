63ec5ab6fa01877f52fff7e5096fa27bcc20ad1b
Michael Hansen <hansen.mike@gmail.com>
2020-02-29 14:52:34 -0500

    Initial service messages (not implemented)

---
+++ rhasspyhermes/service.py
@@ -0,0 +1,30 @@
+"""Messages shared across services."""
+import attr
+
+
+@attr.s(auto_attribs=True, slots=True)
+class ServiceGetStatus(Message):
+    """Get status of a service."""
+
+    categories: typing.List[str] = []
+    id: str = ""
+    siteId: str = "default"
+
+
+@attr.s(auto_attribs=True, slots=True)
+class ServiceProblem:
+    """Description of a problem a service has."""
+
+    problemId: str = ""
+    description: str = ""
+
+
+@attr.s(auto_attribs=True, slots=True)
+class ServiceStatus(Message):
+    """Status of service."""
+
+    category: str = ""
+    status: str = ""
+    problems: typing.List[ServiceProblem] = []
+    id: str = ""
+    siteId: str = "default"
